020b7cd4f091ca8c0fb3267bdd04554352c6449a
Ava <ava.perhaps+git@pm.me>
2023-10-09 22:18:18 -0400

    General refactoring and refinement

---
+++ Chill Populations/Scripts/Patches.cs
@@ -0,0 +1,74 @@
+﻿using HarmonyLib;
+using Qud.API;
+using System;
+using XRL.World;
+using XRL.World.Parts;
+using XRL.World.ZoneBuilders;
+
+namespace Ava.ChillPopulations.HarmonyPatches
+{
+	// warning: here be dragons
+	// many of these methods have inline logic that's pretty hardcoded and there's no clean way to just postfix onto an existing conditional except for thralls
+	// we do the best we can, but it's pretty bad
+
+	[HarmonyPatch(typeof(HasThralls), nameof(HasThralls.IsSuitableThrall))]
+	class ThrallPatch
+	{
+		/// <summary>
+		/// Prevent creatures with the "NoThrall" tag from appearing as psychic thralls.
+		/// Creatures that have a mental shield, like non-sapient oozes, fungi, etc, are skipped already.
+		/// </summary>
+		[HarmonyPostfix]
+		static void IsSuitableThrallPatch(GameObjectBlueprint BP, ref bool __result)
+		{
+			if (__result && BP.HasTagOrProperty("NoThrall"))
+				__result = false;
+		}
+	}
+
+	[HarmonyPatch(typeof(PariahSpawner), nameof(PariahSpawner.GeneratePariah), typeof(int), typeof(bool), typeof(bool))]
+	class PariahPatch
+	{
+		/// <summary>
+		/// Prevent creatures with the "NoPariah" tag from appearing as pariahs.
+		/// We override the base method here, which is quite destructive.
+		/// While it's probably possible to transpile it to include a new filter in place of the old,
+		/// transpilers in general have good potential to break things.
+		/// </summary>
+		[HarmonyPrefix]
+		static bool GeneratePariahOverride(int level, bool AlterName, bool IsUnique, ref GameObject __result)
+		{
+			Predicate<GameObjectBlueprint> filter = c => !c.HasTagOrProperty("NoPariah");
+			GameObject filteredCreature = (level == -1 ? EncountersAPI.GetACreature(filter) : EncountersAPI.GetCreatureAroundLevel(level, filter));
+			__result = PariahSpawner.GeneratePariah(filteredCreature, AlterName, IsUnique);
+			return false;
+		}
+	}
+
+	[HarmonyPatch(typeof(SultanDungeon), "ProcessBlueprintForCultRole")]
+	class HistoricSiteCultistPatch
+	{
+		/// <summary>
+		/// Prevent creatures with the "NoCultist" tag from appearing as cultists in historical sites.
+		/// </summary>
+		[HarmonyPrefix]
+		static bool ProcessBlueprintForCultRolePatch(GameObjectBlueprint B)
+		{
+			return !B.HasTagOrProperty("NoCultist");
+		}
+	}
+
+	[HarmonyPatch(typeof(CultistSpawner), "GetLikedFactionMember")]
+	class TombCultistPatch
+	{
+		/// <summary>
+		/// Prevent creatures with the "NoCultist" tag from appearing as cultists in the Tomb of the Eaters.
+		/// </summary>
+		[HarmonyPrefix]
+		static void GetLikedFactionMemberPrefix(ref Predicate<GameObjectBlueprint> filter)
+		{
+			Predicate<GameObjectBlueprint> f = filter;
+			filter = s => !s.HasTagOrProperty("NoCultist") && f(s);
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # ChillPopulations
 
-This is a Caves of Qud mod that excludes certain disruptive creatures from appearing as villagers converts, psychic thralls, and sultan cultists. You can check the [workshop page](https://steamcommunity.com/sharedfiles/filedetails/?id=2918134876) for a full list of critters affected.
+This is a Caves of Qud mod that excludes certain disruptive creatures from appearing as villagers, converts, psychic thralls, and sultan cultists. You can check the [workshop page](https://steamcommunity.com/sharedfiles/filedetails/?id=2918134876) for a full list of things affected.
 
-This mod is licensed under the [MIT License](LICENSE). Feel free to use all of the code here for learning purposes, although I can't guarantee all of it's clean!
+Chill Populations is licensed under the [GNU General Public License v3](http://www.gnu.org/licenses/agpl.html), which can be found in full in [LICENSE.md](LICENSE.md). Code prior to commit `5221c9c6b332892dca6ce93e95dc1c72594fe9e8` is licensed under the [MIT License](https://opensource.org/license/mit/) instead.
+
+## Changelog
+
+### 9 October, 2023
+Note: 1.1 onwards is licensed under GNU General Public License v3. Versions prior to 1.0 remain licensed under the MIT License.
+
+#### Version 1.1
+* Added chrome idol heroes and pulsed field magnets to the list of barred creatures.
+* General code cleanup and modernization pass.
+
+### 12 September, 2023
+#### Version 1.0.1
+* Added some creatures that were missed in the initial release (snapjaw heroes and goatfolk heroes).
+
+### 2 February, 2023 (version 1.0)
+* Initial release.
